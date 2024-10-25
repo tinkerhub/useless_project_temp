ec71c62ec540733d521b779e4cfd108211831dd5
sandrasubru <sandrasubru607@gmail.com>
2024-10-26 02:08:41 +0530

    content update

---
+++ justifaction-app/src/justifications.js
@@ -0,0 +1,57 @@
+/ src/justifications.js
+
+const justifications = {
+  work: [
+    "I can't come in today; I accidentally glued myself to my office chair while trying to fix a broken stapler.",
+    "I need to take a mental health day; my computer just tried to send me a LinkedIn request, and I'm feeling overwhelmed.",
+    "I have to miss the presentation because I’m currently mediating a dispute between my printer and my scanner about who gets to be the office MVP.",
+    "I'm sorry, but I can’t work today; I’m trying to convince my plants that they really are the best at photosynthesis.",
+    "I’m stuck in a debate with my computer over whether ‘the cloud’ is a real place or just a metaphor.",
+    "I can't attend the meeting; I have a prior commitment to help my cat write its autobiography.",
+    "I’ve been selected to represent our company in a ‘Worst Excuse’ competition, and I can’t let you down.",
+    "I can’t send that report; I’m currently translating it into Morse code for my pet hamster.",
+    "I won’t be in today; my coffee machine has gone on strike, and negotiations are ongoing.",
+    "I have to decline the project because my GPS is convinced I live in Narnia.",
+
+  ],
+  home: [
+     "I can't help with dinner; my fridge is arguing with the microwave about who cooks better.",
+     "I’m unable to clean today; my vacuum cleaner insists it’s on a break and has locked itself in the closet.",
+     "I can’t do laundry; my washing machine is trying to negotiate a better spin cycle.",
+     "I have to stay home; my couch has declared today a national ‘Do Nothing’ day.",
+     "I can't babysit; my goldfish is holding a summit on aquatic diplomacy.",
+     "I need to take the day off to teach my toaster about proper browning techniques.",
+     "I’m stuck in a staring contest with my houseplants; it’s very intense.",
+     "I can’t vacuum; the dust bunnies are threatening to unionize.",
+     "I’m currently training my dog for the next Olympic Games in napping.",
+     "I have to organize my sock drawer; it's become a chaotic ecosystem.",
+  ],
+  college: [
+    "I can’t attend class; my brain is in a heated debate with my textbook about the meaning of life.",
+    "I’m unable to submit my paper; my laptop has started a book club and is currently reading it.",
+    "I can’t make it to the lab; I accidentally turned my roommate’s project into a science fair presentation.",
+    "I’m stuck in a time loop; I keep replaying last week’s lecture on repeat.",
+    "I have to skip class; my coffee just informed me it’s going on strike for better beans.",
+    "I can’t study; my notes have gone on a spontaneous vacation to the Bahamas.",
+    "I won’t be attending; my procrastination coach has scheduled an emergency session.",
+    "I can’t go to the exam; I’m currently training for a competitive napping event.",
+    "I’m in a heated argument with my calculator over the meaning of Pi.",
+    "I can’t attend the group project; my pen has gone rogue and is exploring existentialism.",
+  ],
+  social:[
+    "I can't go out tonight; my cat just discovered the concept of karaoke and insists I perform duets with her.",
+     "I have to cancel; my refrigerator is hosting a motivational speaking event for leftover food.",
+     "I can’t make it; my couch is convinced it’s auditioning for a role in a reality TV show about extreme laziness.",
+    "I’m unable to join; my dog is trying to teach me the art of interpretive dance.",
+    "I can't come to the party; my plants are having a deep discussion about the meaning of life, and I’ve been appointed as the mediator.",
+    "I have to skip dinner; my blender has gone rogue and is trying to start a smoothie business.",
+    "I can’t hang out; my remote control has gone missing, and I need to conduct an intense search.",
+    "I’m stuck in a staring contest with my goldfish; it’s very competitive.",
+    "I have to pass on the gathering; my socks are in a heated argument about who’s the ‘best foot friend’.",
+    "I can’t attend; my teddy bear just informed me he needs emotional support during a crisis.",
+
+
+  ]
+};
+
+export default justifications;
