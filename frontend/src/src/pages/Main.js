41e45a3021be0603d032c1936bb0fe245f27f50d
Akshadha A <95520592+AksA1210@users.noreply.github.com>
2024-11-03 12:57:52 +0530

    Update Main.js

--- frontend/src/src/pages/Main.js
+++ frontend/src/src/pages/Main.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import {Link, useNavigate } from "react-router-dom";
 import "./Main.css";
 
 function Main() {
